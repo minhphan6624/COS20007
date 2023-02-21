96b2db9b5ff2a0e68adf038e76d1c855a279b1d5
minhphan6624 <quangminh6624@gmail.com>
2023-02-21 21:27:39 +1100

    Add Week 1 + 2 work

---
+++ Week1/1.2/HelloWorld/HelloWorld/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	public class Message
+	{
+		private string _text;
+
+		public Message(string atext)
+		{
+			_text = atext;
+		}
+
+		public void Print()
+		{
+			Console.WriteLine(_text);
+		}
+
+	}
+}
+
---
+++ Week1/1.2/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    { 
+        public static void Main(string[] args) 
+        {
+            Message myMessage = new Message("Hello World! - from Message object");
+            myMessage.Print();
+
+            Message[] messages = new Message[5];
+
+            messages[0] = new Message("Welcome Back!");
+            messages[1] = new Message("What a lovely name");
+            messages[2] = new Message("Great name");
+            messages[3] = new Message("Oh hi!");
+            messages[4] = new Message("That is a silly name");
+
+            string name;
+            while (true)
+            {
+                Console.Write("Enter name: ");
+                name = Console.ReadLine();
+
+                if (name.ToLower() == "mark")
+                {
+                    messages[0].Print();
+                }
+                else if (name.ToLower() == "fred")
+                {
+                    messages[1].Print();
+                }
+                else if (name.ToLower() == "wilma")
+                {
+                    messages[2].Print();
+                }
+                else if (name.ToLower() == "alice")
+                {
+                    messages[3].Print();
+                }
+                else
+                {
+                    messages[4].Print();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
